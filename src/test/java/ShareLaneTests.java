0fdffb3855ed56cdf93d6a187c560e001d5002a7
AnnaYedoshina <yedoshina.java@gmail.com>
2023-05-03 20:22:12 +0300

    Added signup tests

--- src/test/java/ShareLaneTests.java
+++ src/test/java/ShareLaneTests.java
@@ -29,8 +29,9 @@ public class ShareLaneTests {
         driver.quit();
 
     }
+
     @BeforeMethod
-    public void navigate(){
+    public void navigate() {
         driver.get(URL);
 
     }
@@ -67,6 +68,73 @@ public class ShareLaneTests {
         Assert.assertTrue(errorMessage.isDisplayed());
         String expectedErrorMessageText = "Oops, error on page. ZIP code should have 5 digits";
         Assert.assertEquals(errorMessage.getText(), expectedErrorMessageText);
+
+
+    }
+
+    @Test
+    public void positiveSignUpTest() {
+        WebElement signUpButton = driver.findElement(By.xpath("//a[@href='./register.py']"));
+        signUpButton.click();
+        WebElement zipCodeInput = driver.findElement(By.cssSelector("input[name=zip_code]"));
+        zipCodeInput.clear();
+        zipCodeInput.sendKeys("12345");
+        WebElement continueButton = driver.findElement(By.cssSelector("input[value=Continue]"));
+        continueButton.click();
+        WebElement firstNameInput = driver.findElement(By.cssSelector("input[name=first_name]"));
+        firstNameInput.clear();
+        firstNameInput.sendKeys("Anna");
+        WebElement lastNameInput = driver.findElement(By.cssSelector("input[name=last_name]"));
+        lastNameInput.clear();
+        lastNameInput.sendKeys("Yedoshina");
+        WebElement emailInput = driver.findElement(By.cssSelector("input[name=email]"));
+        emailInput.clear();
+        emailInput.sendKeys("anna@gmail.com");
+        WebElement passwordInput = driver.findElement(By.cssSelector("input[name=password1]"));
+        passwordInput.clear();
+        passwordInput.sendKeys("12345");
+        WebElement confirmPasswordInput = driver.findElement(By.cssSelector("input[name=password2]"));
+        confirmPasswordInput.clear();
+        confirmPasswordInput.sendKeys("12345");
+        WebElement registerButton = driver.findElement(By.cssSelector("input[value=Register]"));
+        registerButton.click();
+        WebElement confirmationMessage = driver.findElement(By.cssSelector(".confirmation_message"));
+        Assert.assertTrue(confirmationMessage.isDisplayed());
+        String expectedConfirmationMessageText = "Account is created!";
+        Assert.assertEquals(confirmationMessage.getText(), expectedConfirmationMessageText);
+
+
+    }
+
+    @Test
+    public void negativeSignUpTest() {
+        WebElement signUpButton = driver.findElement(By.xpath("//a[@href='./register.py']"));
+        signUpButton.click();
+        WebElement zipCodeInput = driver.findElement(By.cssSelector("input[name=zip_code]"));
+        zipCodeInput.clear();
+        zipCodeInput.sendKeys("12345");
+        WebElement continueButton = driver.findElement(By.cssSelector("input[value=Continue]"));
+        continueButton.click();
+        WebElement firstNameInput = driver.findElement(By.cssSelector("input[name=first_name]"));
+        firstNameInput.clear();
+        WebElement lastNameInput = driver.findElement(By.cssSelector("input[name=last_name]"));
+        lastNameInput.clear();
+        lastNameInput.sendKeys("Yedoshina");
+        WebElement emailInput = driver.findElement(By.cssSelector("input[name=email]"));
+        emailInput.clear();
+        emailInput.sendKeys("anna@gmail.com");
+        WebElement passwordInput = driver.findElement(By.cssSelector("input[name=password1]"));
+        passwordInput.clear();
+        passwordInput.sendKeys("12345");
+        WebElement confirmPasswordInput = driver.findElement(By.cssSelector("input[name=password2]"));
+        confirmPasswordInput.clear();
+        confirmPasswordInput.sendKeys("12345");
+        WebElement registerButton = driver.findElement(By.cssSelector("input[value=Register]"));
+        registerButton.click();
+        WebElement errorConfirmationMessage = driver.findElement(By.cssSelector(".error_message"));
+        Assert.assertTrue(errorConfirmationMessage.isDisplayed());
+        String expectedErrorConfirmationMessageText = "Oops, error on page. Some of your fields have invalid data or email was previously used";
+        Assert.assertEquals(errorConfirmationMessage.getText(), expectedErrorConfirmationMessageText);
 
 
     }
